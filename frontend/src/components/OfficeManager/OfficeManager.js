7ec0d8c16dab6abfb72837599b2d86a2033b52cf
Noam-Yair <noam0906@gmail.com>
2021-04-12 00:32:29 +0300

    added settings button

--- frontend/src/components/OfficeManager/OfficeManager.js
+++ frontend/src/components/OfficeManager/OfficeManager.js
@@ -4,13 +4,16 @@ import "./OfficeManager.css"
 import Calendar from "../Calendar/Calendar.jsx";
 import format from "date-fns/format";
 import Button from "react-bootstrap/Button";
+import settings_img from "./settings.png";
 
 
 class OfficeManager extends Component {
     state = {
         selectedDate: new Date(),
         registersList: [],
+        openSettings: false,
         maxPeople: 20 //maybe we need to save the last value in db and initialize this value accordingly
+
     }
 
     sendUpdateMaxPeople = () =>{
@@ -41,6 +44,19 @@ class OfficeManager extends Component {
         this.sendUpdateMaxPeople();
     }
 
+    openSettings = () =>{
+        if (!this.state.openSettings) {
+            this.setState({
+                openSettings: true,
+            });
+        }
+        else {
+            this.setState({
+                openSettings: false,
+            });
+        }
+    }
+
     render(){
         let temp = {};
         const registersList = this.props.mapRegistersByDay[format(this.state.selectedDate,"dd/MM/yyyy")];
@@ -49,25 +65,25 @@ class OfficeManager extends Component {
                 <div className ="manager_headpage">
                     <h2>office manager page</h2>
                     <div className ="registers_list">
-                    {registersList ? <ol>{(registersList.map((item, index) =><li key={index} className={item.hs ? "black" : "red"}>{item.name}</li>))}</ol>: ''} 
-
+                    {registersList ? <ol>{(registersList.map((item, index) =><li key={index} className={item.hs ? "black" : "red"}>{item.name}</li>))}</ol>: ''}
                     </div>
                 </div>
                 <div className = "calendar_tab">
                     <Calendar onDateClick={this.onDateClick}></Calendar>
-
                 </div>
-                <div>
+                <button onClick={this.openSettings}>
+                    <img src={settings_img}/>
+                </button>
+                {this.state.openSettings ? <div className="settings_tab">
                     <h2>Settings</h2>
                     <p>Number of people allowed in the office</p>
                     <input className="forms" type="text" placeholder=" " onChange={this.handleChange} value={this.state.maxPeople}></input>
-                     <div>
-                    <Button variant="primary" size="sm" onClick={this.onSaveClick}>
-                        Save
-                    </Button>
-                     </div>
-                </div>
-                    
+                    <div>
+                        <Button variant="primary" size="sm" onClick={this.onSaveClick}>
+                            Save
+                        </Button>
+                    </div>
+                </div> : null}
             </div>
         )
     }
