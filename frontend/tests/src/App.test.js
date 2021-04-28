3eaa09a0762e5c46fc19897d06a54ca32a634241
Avivex1000 <Avivex1000@gmail.com>
2021-04-29 02:40:14 +0300

    irrelevant changes

--- frontend/tests/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import {App} from "src/App"
-
-describe("Filter function", () => {
-  test("it should filter by a search term (link)", () => {
-    const user_copy = { user: 1, email: "https://www.url1.dev" };
-    App.addUser(user_copy)
-
-  });
-});
